b7e16411fa48e17bce6c58abe92c66a5f88de7bb
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2023-08-21 13:41:45 +0600

    name add done

---
+++ js/jsfiles.js
@@ -0,0 +1,72 @@
+// cart 1 calculation
+document.getElementById("cart-1").addEventListener("click",function(){
+    // const cartPrice=StringFromValue('card-1-price');
+    const newList = "K. Accessories";
+
+    const addComment = document.getElementById('list-items');
+    const li = document.createElement('li');
+
+    li.innerText=newList;
+    addComment.appendChild(li);
+    newList.innerText ='';
+});
+// cart 2 calculation
+document.getElementById("cart-2").addEventListener("click",function(){
+    // const cartPrice=StringFromValue('card-2-price');
+    const newList2 = "K. Accessories";
+
+    const addComment = document.getElementById('list-items');
+    const li = document.createElement('li');
+
+    li.innerText=newList2;
+    addComment.appendChild(li);
+    newList2.innerText ='';
+});
+// cart 3 calculation
+document.getElementById("cart-3").addEventListener("click",function(){
+    // const cartPrice=StringFromValue('card-3-price');
+    const newList3 = "Home Cooker";
+
+    const addComment = document.getElementById('list-items');
+    const li = document.createElement('li');
+
+    li.innerText=newList3;
+    addComment.appendChild(li);
+    newList3.innerText ='';
+});
+// cart 4 calculation
+document.getElementById("cart-4").addEventListener("click",function(){
+    // const cartPrice=StringFromValue('card-4-price');
+    const newList4 = "Sports Back Cap";
+
+    const addComment = document.getElementById('list-items');
+    const li = document.createElement('li');
+
+    li.innerText=newList4;
+    addComment.appendChild(li);
+    newList4.innerText ='';
+});
+// cart 5 calculation
+document.getElementById("cart-5").addEventListener("click",function(){
+    // const cartPrice=StringFromValue('card-5-price');
+    const newList5 = "Full Jersey Set";
+
+    const addComment = document.getElementById('list-items');
+    const li = document.createElement('li');
+
+    li.innerText=newList5;
+    addComment.appendChild(li);
+    newList5.innerText ='';
+});
+// cart 6 calculation
+document.getElementById("cart-6").addEventListener("click",function(){
+    const newList6 = "Sports cates ";
+
+    const addComment = document.getElementById('list-items');
+    const li = document.createElement('li');
+
+    li.innerText=newList6;
+    addComment.appendChild(li);
+    newList6.innerText ='';
+});
+
---
+++ js/utilites.js
@@ -0,0 +1,17 @@
+// function StringFromValue(ElementId) {
+//     const elementSelection = document.getElementById(ElementId);
+//     const gettingElementStringValue = elementSelection.innerText;
+//     const ElementValue = parseFloat(gettingElementStringValue);
+
+
+
+//     return ElementValue;
+    
+
+// }
+
+
+// function totalPrice (previousPrice , currentTotalPrice){
+//     const totalPriceWithoutDiscount= previousPrice+currentTotalPrice;
+//     return totalPriceWithoutDiscount;
+// }
