1fb6787c8ca9ff9a49da9f113f08ba1f98fb3740
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2023-08-21 17:24:26 +0600

    calculation done

---
+++ js/totalCalculation.js
@@ -0,0 +1,113 @@
+document.getElementById("cart-1").addEventListener("click",function(){
+    const cartValue = stringToFloatValue('card-1-price');
+    const totalPrePrice = stringToFloatValue('Total-Price');
+    const total =totalPrice (cartValue , totalPrePrice);
+    changingInnerTextById ('Total-Price' , total);
+    // const discountWillBe = parseFloat(total/5);
+    // if(total>200 ){ 
+    //     changingInnerTextById ('discount' , discountWillBe);
+    // }
+    changingInnerTextById ('final-total' , total);
+    updateButtonStatus2(total);
+    updateButtonStatus(total);
+    
+});
+
+// cart 2
+
+document.getElementById("cart-2").addEventListener("click",function(){
+    const cartValue = stringToFloatValue('card-2-price');
+    const totalPrePrice = stringToFloatValue('Total-Price');
+    const total =totalPrice (cartValue , totalPrePrice);
+    changingInnerTextById ('Total-Price' , total);
+    // const discountWillBe = parseFloat(total/5);
+    // if(total>200 ){ 
+    //     changingInnerTextById ('discount' , discountWillBe);
+    // }
+    changingInnerTextById ('final-total' , total);
+    updateButtonStatus2(total);
+    updateButtonStatus(total);
+})
+
+// cart 3
+
+document.getElementById("cart-3").addEventListener("click",function(){
+    const cartValue = stringToFloatValue('card-3-price');
+    const totalPrePrice = stringToFloatValue('Total-Price');
+    const total =totalPrice (cartValue , totalPrePrice);
+    changingInnerTextById ('Total-Price' , total);
+    // const discountWillBe = parseFloat(total/5);
+    // if(total>200 ){ 
+    //     changingInnerTextById ('discount' , discountWillBe);
+    // }
+    changingInnerTextById ('final-total' , total);
+    updateButtonStatus2(total);
+    updateButtonStatus(total);
+})
+
+// cart 4
+
+document.getElementById("cart-4").addEventListener("click",function(){
+    const cartValue = stringToFloatValue('card-4-price');
+    const totalPrePrice = stringToFloatValue('Total-Price');
+    const total =totalPrice (cartValue , totalPrePrice);
+    changingInnerTextById ('Total-Price' , total);
+    // const discountWillBe = parseFloat(total/5);
+    // if(total>200 ){ 
+    //     changingInnerTextById ('discount' , discountWillBe);
+    // }
+    changingInnerTextById ('final-total' , total);
+    updateButtonStatus2(total);
+    updateButtonStatus(total);
+})
+
+// cart 5
+
+document.getElementById("cart-5").addEventListener("click",function(){
+    const cartValue = stringToFloatValue('card-5-price');
+    const totalPrePrice = stringToFloatValue('Total-Price');
+    const total =totalPrice (cartValue , totalPrePrice);
+    changingInnerTextById ('Total-Price' , total);
+    // const discountWillBe = parseFloat(total/5);
+    // if(total>200 ){ 
+    //     changingInnerTextById ('discount' , discountWillBe);
+    // }
+    changingInnerTextById ('final-total' , total);
+    updateButtonStatus2(total);
+    updateButtonStatus(total);
+})
+
+
+// cart 6 
+document.getElementById("cart-6").addEventListener("click",function(){
+    const cartValue = stringToFloatValue('card-6-price');
+    const totalPrePrice = stringToFloatValue('Total-Price');
+    const total =totalPrice (cartValue , totalPrePrice);
+    changingInnerTextById ('Total-Price' , total);
+    const discountWillBe = parseFloat(total/5);
+    // if(total>200 ){ 
+    //     changingInnerTextById ('discount' , discountWillBe);
+    // }
+    changingInnerTextById ('final-total' , total);
+    updateButtonStatus2(total);
+    updateButtonStatus(total);
+})
+
+// mew button 
+document.getElementById('applyButton').addEventListener('click',function(){
+    const total = parseFloat(document.getElementById('Total-Price').innerText);
+    const discountWillBe = parseFloat(document.getElementById('discount').innerText);
+
+     const inputFromApply= document.getElementById('input-field');
+     const inputValue = inputFromApply.value;
+     if( inputValue==='SELL200' ){ 
+        const discountWillBe = parseFloat(total/5);
+        changingInnerTextById ('discount' , discountWillBe);
+
+        const finalPrice = total -discountWillBe;
+
+
+    changingInnerTextById ('final-total' , finalPrice);
+
+     }
+})
--- js/utilites.js
+++ js/utilites.js
@@ -1,17 +1,38 @@
-// function StringFromValue(ElementId) {
-//     const elementSelection = document.getElementById(ElementId);
-//     const gettingElementStringValue = elementSelection.innerText;
-//     const ElementValue = parseFloat(gettingElementStringValue);
+function stringToFloatValue(elementId){
+    const selectedElement=document.getElementById(elementId);
+    const valueInString = selectedElement.innerText;
+    const value =parseFloat(valueInString);
+
+    return value;
+}
 
 
+function totalPrice (previousPrice , currentTotalPrice){
+    const totalPriceWithoutDiscount= previousPrice+currentTotalPrice;
+    return totalPriceWithoutDiscount;
+}
 
-//     return ElementValue;
-    
+function changingInnerTextById (elementId , value){
+    const textElement = document.getElementById(elementId);
+     textElement.innerText = value;
+}
 
-// }
+// purchase button 
+const button = document.getElementById("purchasebutton");
+function updateButtonStatus(price) {
+    if (price > 0) {
+      button.removeAttribute("disabled"); // Enable the button
+    } else {
+      button.setAttribute("disabled", "true"); // Disable the button
+    }
+  }
 
-
-// function totalPrice (previousPrice , currentTotalPrice){
-//     const totalPriceWithoutDiscount= previousPrice+currentTotalPrice;
-//     return totalPriceWithoutDiscount;
-// }
+//   apply button 
+const button2 = document.getElementById("applyButton");
+function updateButtonStatus2(price2) {
+    if (price2 >200 ) {
+      button2.removeAttribute("disabled"); // Enable the button
+    } else {
+      button2.setAttribute("disabled", "true"); // Disable the button
+    }
+  }
